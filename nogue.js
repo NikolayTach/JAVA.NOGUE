c92d09596fe6b859f54f49d2959f8b5636364d4f
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-03-26 03:31:57 -0700

    Update nogue.js

--- nogue.js
+++ nogue.js
@@ -7,12 +7,12 @@ import JAVA.NOGUE(!1) function
     {
 A brings '11' and '22' 
 A by 1 number seq in JAVA.NOGUE
-take gor = brings [JAVA!NOGUE.JAVA!NOGUE2.JAVA!NOGUE3.AVA!NOGUE4.JAVA!NOGUE5.JAVA!NOGUE6]
+take gor = brings [A/JAVA!NOGUE.A/JAVA!NOGUE2.A/JAVA!NOGUE3.A/JAVA!NOGUE4.A/JAVA!NOGUE5.A/JAVA!NOGUE6]
 gor = 3 As B(11122) popups (nosign!!)
 gor = form !B folowing brings '11'
 B brings '22' and '11' 
 B by 1 number seq in JAVA.NOGUE(!1)
-take gor = brings [JAVA.NOGUE(!1).JAVA.NOGUE(!1)2.JAVA.NOGUE(!1)3.JAVA.NOGUE(!1)4.JAVA.NOGUE(!1)5.JAVA.NOGUE(!1)6]
+take gor = brings [B/JAVA.NOGUE(!1).B/JAVA.NOGUE(!1)2.B/JAVA.NOGUE(!1)3.B/JAVA.NOGUE(!1)4.B/JAVA.NOGUE(!1)5.B/JAVA.NOGUE(!1)6]
 			 
 
 }
