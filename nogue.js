10e22d5201653e4fe648de183c0f86fe93ccdf77
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-03-26 07:39:41 +0000

    Update nogue.js

--- nogue.js
+++ nogue.js
@@ -1,7 +1,19 @@
 import function QACX.yml
 import function m21.yml
 import function ADSCVIVE.ts
-{ 
+import AB function 
+import AB(1) function
+{
+    {
+A brings '11' and '22' 
+A by 1 number seq in .A
+take gor = brings [A.A.A.A.A.A]
+gor = 3 As B(11122) popups (nosign!!)
+gor = form !B folowing brings '11'
+B brings '22' and '11' 
+B by 1 number seq in .B
+take gor = brings [B.B.B.B.B.B]
+			    }	
 loop range as foward a beta "java.nogue.io"
      freqent request file nogue
    }
