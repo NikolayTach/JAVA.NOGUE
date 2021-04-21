131ea40572e0627ecb8dd3a22be6782725bc757e
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-04-21 18:05:12 +0300

    Add files via upload

---
+++ binary.nogue.js
@@ -0,0 +1,27 @@
+import function nogueTESTpiece.java 
+import function GUInogueTEST.java
+import function nogue.js
+{
+   {
+Aimed at frome -- java.nogue[binary.nogue]
+}
+@log_nogue
+  Do form
+   acts
+    one == 11!
+#Link was adddresed to a 10B stke of file packets that what opened on Zabbix server range line.#
+
+{
+A brings '11' and '22' 
+gor = 3 As B(JDK16.Open.Packs) popups (nosign!!)
+gor = form !B folowing brings '11'
+
+}
+{
+	{
+Level 1(adverb&3)def == 1 B per min loaded in JDK16.Open.Packs/JAVA.NOGUE/bobo1.c
+Level 1(adverb&3)def == 1 B per min loaded in JDK16.Open.Service.Packs/JAVA.NOGUE/bobo1.c
+Level 1(adverb&3)def == 1 B per min loaded in JDK16.Open.Jet.Packs/JAVA.NOGUE/bobo1.c
+Level 1(adverb&3)def == 1 B per min loaded in JDK16.Open.Wig.Packs/JAVA.NOGUE/bobo1.c
+	}
+}
