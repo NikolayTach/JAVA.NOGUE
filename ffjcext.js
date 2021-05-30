bb23695f5c44c7228df1c6f91a7c6808be5d14bb
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-05-30 13:09:46 +0000

    Add files via upload

---
+++ ffjcext.js
@@ -0,0 +1,42 @@
+const gJavaConsole1_8_0_291 =  {
+
+	id	: "javaconsole1.8.0_291",
+
+	mimeType: "application/x-java-applet;jpi-version=1.8.0_291",
+
+	install	: function() {
+		window.addEventListener("load",this.init,false);
+	},
+
+	init	: function() { 
+		if (navigator.mimeTypes[gJavaConsole1_8_0_291.mimeType]) {
+			var toolsPopup = document.getElementById("menu_ToolsPopup");	
+			toolsPopup.addEventListener("popupshowing",gJavaConsole1_8_0_291.enable,false);
+			var element = document.getElementById(gJavaConsole1_8_0_291.id);
+			element.setAttribute( "oncommand" , "gJavaConsole1_8_0_291.show();");
+		} else {
+			var element = document.getElementById(gJavaConsole1_8_0_291.id);
+			element.setAttribute("style", "display: none");
+		}
+	},
+
+	enable	: function() {
+		var element = document.getElementById(gJavaConsole1_8_0_291.id);
+    		if (navigator.javaEnabled()) {
+			element.removeAttribute("disabled");
+    		} else {
+      			element.setAttribute("disabled", "true");
+    		}
+	},
+
+	show	: function() {
+     		var jvmMgr = Components.classes['@mozilla.org/oji/jvm-mgr;1']
+	                   .getService(Components.interfaces.nsIJVMManager)
+    		jvmMgr.showJavaConsole();
+	}
+	
+};
+
+gJavaConsole1_8_0_291.install();
+
+
