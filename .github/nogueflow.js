e460fd2ec8980af1095c2e0b556288d058f1e40f
NikolayTach <NikolayTach@users.noreply.github.com>
2021-07-30 17:12:11 +0300

    Update nogueflow.js
    G

---
+++ .github/nogueflow.js
@@ -0,0 +1,6 @@
+import from nogue.js
+{whole dir set for [A] or as [B] nogue.loop(set) 
+AssertRawFunction too null == 1:1 log in gor = form !B folowing brings '11'
+B brings '22' and '11' 
+B by 1 number seq in JAVA.NOGUE(!1)
+take gor = brings 1:1
