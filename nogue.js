92e837f8f2ce4e5b7dca924d88458a27da9bac0d
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-08-02 14:08:07 +0300

    Add files via upload

---
+++ nogue.js
@@ -0,0 +1,17 @@
+import function QACX.yml
+import function m21.yml
+import function ADSCVIVE.ts
+import JAVA.NOGUE function 
+import JAVA.NOGUE(!1) function
+
+A by 1 number seq in JAVA.NOGUE
+take gor = brings [A:/JAVA!NOGUE.A/JAVA!NOGUE2.A/JAVA!NOGUE3.A/JAVA!NOGUE4.A/JAVA!NOGUE5.A/JAVA!NOGUE6]
+{
+	#figure diagnostics = null
+	#cloud_domain = nikolai.java@nogue.io
+	#Oracle_keyJS.log0 = 2164012816_20131329466
+
+	    }
+   {
+
+	    {			 
