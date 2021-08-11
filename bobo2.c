34a1b9356320c54c5a09d09a2189914fa4f74498
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-08-11 19:30:26 +0300

    Add files via upload

---
+++ bobo2.c
@@ -0,0 +1,14 @@
+#include<bobo1.c>
+
+{
+	block log intro file /JAVA.NOGUE-main/JDK16.io.Open.Service.Packs
+int forum run 
+	to 
+		dir 
+	"/dsm/sa23/124441251/"
+	"/dsm/sa23/122332324/"
+	"/dsm/sa23/212341242/"
+sda 
+brings to null
+reach 
+	}
