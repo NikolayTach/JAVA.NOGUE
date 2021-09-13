07d4a2a76f7132328657b7376277cbd26e67bf43
NikolayTach <NikolayTach@users.noreply.github.com>
2021-09-13 11:07:42 +0300

    bobo1.c Update compilation

--- JDK16.io.Open.Wig.Packs/bobo1.c
+++ JDK16.io.Open.Wig.Packs/bobo1.c
@@ -6,15 +6,25 @@
      -b (function) brings vol%6
 'SF144' NULL
 do range  int -b 
-  'kolli' do NULL 
-    -a "/main/xop-master/bobo1.c"
-         while arg 'SF144' sooked del 'SF1123' for @  queary search -i  asc "java.nogue.io" load int  
-for  range NULL "main/xop-master/bobo1" so queary 
+   do NULL 
+   bobo1 -a "/main/xop-master/bobo1.c"
+         while arg 'SF144' sooked del 'SF1123' for 
+ bobo1 search -i  asc "java.nogue.io" load int  
+for  range NULL "/main/xop-master/bobo1.c" 
+gpg fingerprint 
+to "JAVA.NOGUE-main/CB6A3D6E6E86F19818F7293B84494D2C449F04AD.acs" so queary 
 arg int 
-  move face to v0:1616743818.9542642
+  from bobo1.sh 
+  move face to 
+"JAVA.NOGUE-main/JDK16.io.Open.Service.Packs"
+"JAVA.NOGUE-main/JDK16.io.Open.Jet.Packs"
+"JAVA.NOGUE-main/Java_SE_15.Open.Service.Packs"
+"JAVA.NOGUE-main/Java_SE_15.Open.Jet.Packs"
+"JAVA.NOGUE-main/Java_SE_15.Open.Wig.Packs"
 Done!
  }
    {
-number //-a , -b,-f ,-l ,-i//  
+bobo1_command -a[arange default file]
+,-b[binary search default file],-f[,-l [link package],-i [search default file]  //  
 
 }
