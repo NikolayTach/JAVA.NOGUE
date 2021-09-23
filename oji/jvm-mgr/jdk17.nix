a444e53c6aeec4f0cc647759783c8a2bdc4a75fb
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-09-23 08:14:50 +0000

    Update jdk17.nix

--- oji/jvm-mgr/jdk17.nix
+++ oji/jvm-mgr/jdk17.nix
@@ -4,7 +4,7 @@ nixpkgs.config = {
 };
 environment.systemPackages = with pkgs; [ discord ]
 	{
- public.compound.Write.Full()
+ public.compound.Write.Full(24)
 manage_set = set.value(24)
 	#gpg-key = "nikolai.java@nogue.io"
 	 #ref = "/nix/store/f1aq2j7v51w4i7z7h05lij3ri7k4mw46-nix-2.3.12/share/nix/sandbox/sandbox-defaults.sb"
