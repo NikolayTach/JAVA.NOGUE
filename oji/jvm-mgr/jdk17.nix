c90007c7f606e3677fe35f916920114d67872cd3
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-09-23 08:18:19 +0000

    Update jdk17.nix

--- oji/jvm-mgr/jdk17.nix
+++ oji/jvm-mgr/jdk17.nix
@@ -2,7 +2,8 @@
 	{
  public.compound.Write.Full = true
  };
- {
+	n/
+	{
 manage.set = set.value(24)
 	#gpg-key = "nikolai.java@nogue.io"
 	 #ref = "/nix/store/f1aq2j7v51w4i7z7h05lij3ri7k4mw46-nix-2.3.12/share/nix/sandbox/sandbox-defaults.sb"
